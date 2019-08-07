5099f4c1080e3093d0e23568381d57302c242ce6
rhn014 <Roland.Hyden@RegionHalland.se>
2019-08-07 11:35:06 +0200

    Lagt till kontroll av -env-värden

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# region-halland-admin-page-styleguide
-Registerar css resp js för admin
+# Lägg till css/js i admin och/eller en css för wordpress editor
+
+## Hur man använder Region Hallands plugin "region-halland-admin-page-styleguide"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-admin-page-styleguide".
+
+
+## Användningsområde
+
+Denna plugin kan lägga till css/js i admin och/eller en css för wordpress editor
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-admin-page-styleguide.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-admin-page-styleguide.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-admin-page-styleguide": "1.0.0"
+},
+```
+
+
+## Konfiguerar sökvägar via .env-filen
+
+Du anger sökväg till css/js resp namn för editor-css i .env-filen via
+
+Admin css
+```sh
+ADMIN_CSS_PATH=${WP_HOME}/web/[SÖKVÄG TILL CSS-FIL].css
+```
+
+Admin js
+```sh
+ADMIN_JS_PATH=${WP_HOME}/web/[SÖKVÄG TILL JS-FIL].js
+```
+
+Wordpress editor (tinyMCE)
+```sh
+EDITOR_STYLE_NAME=[NAMN PÅ CSS-FIL].css
+```
+
+OBS! För editor css måste filen ligga i mappen web/app/themes/[DITT TEMA]/resources.
+Är filen placerad i den mappen hittar wordpress editor automatiskt filen
+
+
+
+## Versionhistorik
+
+### 1.2.0
+- Lagt till kontroller om det finns angivna värden för respektive fil
+- Finns inte env-variablerna angivna används inte css/js i admin
+
+### 1.1.0
+- Lagt till stöd för editor tinyMCE
+
+### 1.0.0
+- Första version
--- region_halland_admin_page_styleguide.php
+++ region_halland_admin_page_styleguide.php
@@ -5,46 +5,73 @@
 	 */
 	/*
 	Plugin Name: Region Halland Admin Page Styleguide
-	Description: Styleguide för en page i admin
-	Version: 1.1.0
+	Description: Lägg till css/js i admin och/eller en css för wordpress editor
+	Version: 1.2.0
 	Author: Roland Hydén
 	License: MIT
 	Text Domain: regionhalland
 	*/
 
-	// Anropa metod för att lägga till js-fil
-	add_action('admin_enqueue_scripts', 'my_enqueue_for_pages_styleguide');
+	// Anropa metod för att lägga till css/js-filer
+	add_action('admin_enqueue_scripts', 'my_enqueue_for_region_halland_admin_page_styleguide');
 
-	// Metod för att lägga till js-fil
-	function my_enqueue_for_pages_styleguide($hook) {
+	// Metod för att lägga till css/js-filer
+	function my_enqueue_for_region_halland_admin_page_styleguide($hook) {
+		
+		// Kontrollera om det finns en angiven css för adminb
+		$myAdminCss = ENV('ADMIN_CSS_PATH');
+		$useMyAdminCss = 0;
+		if (strlen($myAdminCss) > 0) {
+			$useMyAdminCss = 1;
+		}
+		
+		// Kontrollera om det finns en angiven js för adminb
+		$myAdminJs = ENV('ADMIN_JS_PATH');
+		$useMyAdminJs = 0;
+		if (strlen($myAdminJs) > 0) {
+			$useMyAdminJs = 1;
+		}
+
+		// Kontrollera om det finns en angiven css för wordpress editor
+		$myEditorStyle = ENV('EDITOR_STYLE_NAME');
+		$useMyEditorStyle = 0;
+		if (strlen($myEditorStyle) > 0) {
+			$useMyEditorStyle = 1;
+		}
 		
 		// Styleguide minified css
-		wp_register_style('custom_wp_admin_css_styleguide', env('ADMIN_CSS_PATH'));
-        wp_enqueue_style('custom_wp_admin_css_styleguide');
+		if ($useMyAdminCss == 1) {
+			wp_register_style('custom_wp_admin_css_styleguide', $myAdminCss);
+	        wp_enqueue_style('custom_wp_admin_css_styleguide');
+		}
 
         // Styleguide compiled js
-		wp_register_script('custom_wp_admin_js_styleguide', env('ADMIN_JS_PATH'));
-        wp_enqueue_script('custom_wp_admin_js_styleguide');
+		if ($useMyAdminJs == 1) {
+			wp_register_script('custom_wp_admin_js_styleguide', $myAdminJs);
+	        wp_enqueue_script('custom_wp_admin_js_styleguide');
+		}
 		
 		// TinyMCE editor styling
-		add_editor_style(env('EDITOR_STYLE_NAME'));
+		if ($useMyEditorStyle == 1) {
+			add_editor_style($myEditorStyle);
+		}
 	
 	}
 	
 	// Metod som anropas när pluginen aktiveras
-	function region_halland_admin_page_style_activate() {
+	function region_halland_admin_page_styleguide_activate() {
 		// Ingenting just nu...
 	}
 
 	// Metod som anropas när pluginen avaktiveras
-	function region_halland_admin_page_style_deactivate() {
+	function region_halland_admin_page_styleguide_deactivate() {
 		// Ingenting just nu...
 	}
 	
 	// Vilken metod som ska anropas när pluginen aktiveras
-	register_activation_hook( __FILE__, 'region_halland_admin_page_style_activate');
+	register_activation_hook( __FILE__, 'region_halland_admin_page_styleguide_activate');
 
 	// Vilken metod som ska anropas när pluginen avaktiveras
-	register_deactivation_hook( __FILE__, 'region_halland_admin_page_style_deactivate');
+	register_deactivation_hook( __FILE__, 'region_halland_admin_page_styleguide_deactivate');
 
 ?>
